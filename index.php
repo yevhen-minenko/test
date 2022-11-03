e182532249ab19b173a779b92bb2bdeaa9087899
Yevhen Minenko <yevhen.minenko@architechlabs.com>
2022-11-03 17:30:15 +0100

    1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Task;
+
+require __DIR__ . '/vendor/autoload.php';
+
+return new Task;
---
+++ src/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Bref\Context\Context;
+use Bref\Event\Handler;
+
+class Task implements Handler
+{
+    public function handle(
+        $event,
+        Context $context
+    ): array {
+        return $event;
+    }
+}
